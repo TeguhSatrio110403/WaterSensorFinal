08fcd682f7d7f79b1a12b1b43826f3fb192563d8
TeguhSatrio110403 <tsatrio52@gmail.com>
2024-06-18 20:21:45 +0700

    final

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  useMap,
+} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Card } from 'react-bootstrap';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import HookMqtt from '../service/hook'
+
+const RecenterAutomatically = ({ lat, lng }) => {
+  const map = useMap();
+  useEffect(() => {
+    map.setView([lat, lng]);
+  }, [lat, lng, map]);
+  return null;
+};
+
+const Dashboard = () => {
+  const [position, setPosition] = useState([-6.34605, 106.69156]);
+  const [history, setHistory] = useState([]);  
+
+  return (
+    <div>
+      <section>
+        <h1><center>Maps</center></h1>
+        <div className="maps-box" style={{ height: '600px' }}>
+          <div className="details" id="map" style={{ height: '580px' }}>
+            <MapContainer
+              center={position}
+              zoom={100}
+              scrollWheelZoom={true}
+              style={{ height: '100%', width: '100%' }}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={position} />
+              <RecenterAutomatically lat={position[0]} lng={position[1]} />
+              {history.map((pos, idx) => (
+                <Marker key={idx} position={pos} />
+              ))}
+            </MapContainer>
+          </div>
+        </div>
+
+        <div className="app">
+          <HookMqtt/>
+        </div>
+      </section>
+    
+    </div>
+  );
+};
+
+export default Dashboard;
